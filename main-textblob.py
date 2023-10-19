6811401c9bff2fae89f61be152e7bc706b6df93a
Robin <87436238+epi55@users.noreply.github.com>
2023-10-19 14:24:45 -0700

    Update main-textblob.py

--- main-textblob.py
+++ main-textblob.py
@@ -1,23 +1,14 @@
 from textblob import TextBlob # PIP required (preferred installer program)
-from pypdf import PdfReader # PIP required
+from pypdf import PdfReader # PIP required (pip install pypdf[full])
 import os
 
-my_file = (r"Projects\Sentiment analysis\Sentiment-analysis-example.pdf")
+my_file = (r"Projects\Sentiment analysis\Sentiment-analysis-example-2.pdf")
 
 # SETUP ENGINE
 if os.path.splitext(my_file)[1] == '.pdf':
     reader = PdfReader(my_file)
     page = reader.pages[0]
-    print(page.extract_text())
-    #pdf_file = open(my_file, 'rb')
-    #pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-    #text = ''
-
-    #for page in range(pdf_reader.getNumPageS()):
-    #    text += pdf_reader.getPage(page).extractText()
-
-    #with open(my_file, 'w') as file:
-    #    file.write(text)
+    data = page.extract_text()
 
 if os.path.splitext(my_file)[1] == '.txt':
     with open(my_file, 'r') as file:
@@ -25,19 +16,20 @@ if os.path.splitext(my_file)[1] == '.txt':
         data = data.replace('\n', '')
         data = '\n'.join([line for line in data.split('\n') if not line.startswith('#')])
 
-text = data
-
 # ANALYSIS ENGINE
-polValue = TextBlob(text).sentiment.polarity
-subValue = TextBlob(text).sentiment.subjectivity
+polValue = TextBlob(data).sentiment.polarity
+subValue = TextBlob(data).sentiment.subjectivity
 
 # Polarity (-1:+1)
 # Value of example = -0.008341076267905536
 # -1 (Negative statement)
+# 0 (Neutral statement)
 # +1 (Positive statement)
 print("Polarity of Text is: {}.".format(polValue))
 
 # Subjectivity (0:+1)
 # Value of example = 0.4121157568718545
-# 
+# 0.0 (Objective statement)
+# 0.5 ?
+# 1.0 (Subjective statement)
 print("Subjectivity of Text is: {}.".format(subValue))
